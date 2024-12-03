766165a191afc84b5e5611319520b49f380ca72f
nastjaz <nz1073@student.uni-lj.si>
2024-12-03 20:15:21 +0100

    image size

--- source/Code/ImageSize.py
+++ source/Code/ImageSize.py
@@ -1,7 +1,9 @@
 import nibabel as nib
 
 # Naloži datoteko
-image = nib.load("../ThoraxCBCT/imagesTr/ThoraxCBCT_0000_0000.nii.gz")
+# image = nib.load("../ThoraxCBCT/imagesTr/ThoraxCBCT_0000_0000.nii.gz")
+# data = image.get_fdata()
+image = nib.load("source/Data/image_A.nii.gz")
 data = image.get_fdata()
 
 # Preveri dimenzije
