8f6907af86486018b4b2288eb2185a906e174032
nastjaz <nz1073@student.uni-lj.si>
2024-12-18 16:21:47 +0100

    show

--- source/Code/ImageShow.py
+++ source/Code/ImageShow.py
@@ -28,6 +28,9 @@ except Exception as e:
     print(f"Napaka pri nalaganju slike: {e}")
     raise
 
+print("Moving image stats before resampling:", np.min(moving_img_nii.get_fdata()), np.max(moving_img_nii.get_fdata()))
+print("Warped image stats before resampling:", np.min(warped_img_nii.get_fdata()), np.max(warped_img_nii.get_fdata()))
+
 # Poravnaj slike na podlagi "fixed" slike
 aligned_moving_img = resample_from_to(moving_img_nii, fixed_img_nii)
 aligned_warped_img = resample_from_to(warped_img_nii, fixed_img_nii)
@@ -36,6 +39,10 @@ aligned_warped_img = resample_from_to(warped_img_nii, fixed_img_nii)
 fixed_img = fixed_img_nii.get_fdata()
 aligned_moving_img_data = aligned_moving_img.get_fdata()
 aligned_warped_img_data = aligned_warped_img.get_fdata()
+
+print("Fixed image shape:", fixed_img.shape)
+print("Moving image shape:", aligned_moving_img_data.shape)
+print("Warped image shape:", aligned_warped_img_data.shape)
 
 # Preveri, da imajo vse slike enako število dimenzij
 if len(fixed_img.shape) != 3 or len(aligned_moving_img_data.shape) != 3 or len(aligned_warped_img_data.shape) != 3:
@@ -54,20 +61,21 @@ warped_slice = aligned_warped_img_data[:, :, slice_index]
 fig, ax = plt.subplots(1, 3, figsize=(15, 5))
 
 # Prikaz "fixed" slike
-ax[0].imshow(fixed_slice.T, cmap='gray', origin='lower')
+ax[0].imshow(fixed_slice.T, cmap='gray', vmin=0, vmax=1, origin='lower')
 ax[0].set_title('Fixed Image')
 ax[0].axis('off')
 
 # Prikaz "moving" slike
-ax[1].imshow(moving_slice.T, cmap='gray', origin='lower')
+ax[1].imshow(moving_slice.T, cmap='gray', vmin=0, vmax=1, origin='lower')
 ax[1].set_title('Moving Image')
 ax[1].axis('off')
 
 # Prikaz "warped" (poravnane) slike
-ax[2].imshow(warped_slice.T, cmap='gray', origin='lower')
+ax[2].imshow(warped_slice.T, cmap='gray', vmin=0, vmax=1, origin='lower')
 ax[2].set_title('Warped Image')
 ax[2].axis('off')
 
 # Pokaži vse slike
 plt.tight_layout()
-plt.show()
+plt.show(block=True)
+
